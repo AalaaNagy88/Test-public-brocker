3207aca11387d447207bf1264a18c3a3175c9456
unknown <salaanagy4444@gmail.com>
2019-11-28 03:06:58 +0200

    Test M2Mqtt.lib to acess Cloudmqtt online Brocker

---
+++ Test public brocker/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Test_public_brocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.X = new System.Windows.Forms.TextBox();
+            this.Y = new System.Windows.Forms.TextBox();
+            this.Z = new System.Windows.Forms.TextBox();
+            this.send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // X
+            // 
+            this.X.Location = new System.Drawing.Point(31, 24);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(52, 20);
+            this.X.TabIndex = 0;
+            // 
+            // Y
+            // 
+            this.Y.Location = new System.Drawing.Point(117, 24);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(52, 20);
+            this.Y.TabIndex = 1;
+            // 
+            // Z
+            // 
+            this.Z.Location = new System.Drawing.Point(199, 24);
+            this.Z.Name = "Z";
+            this.Z.Size = new System.Drawing.Size(44, 20);
+            this.Z.TabIndex = 2;
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(105, 77);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 3;
+            this.send.Text = "Send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 167);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.Z);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.X);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox X;
+        private System.Windows.Forms.TextBox Y;
+        private System.Windows.Forms.TextBox Z;
+        private System.Windows.Forms.Button send;
+    }
+}
+
---
+++ Test public brocker/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace Test_public_brocker
+{
+    public partial class Form1 : Form
+    {
+        MqttClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new MqttClient("tailor.cloudmqtt.com", 17519, false, null, null, MqttSslProtocols.None);
+            client.Connect(Guid.NewGuid().ToString(), "zndjtezy", "Midx-b_ZcBKB");
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            client.MqttMsgPublished += client_MqttMsgPublished;
+            ushort msgId = client.Publish("test/", Encoding.UTF8.GetBytes(X.Text +","+ Y.Text +","+ Z.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+        }
+        private static void client_MqttMsgPublished(object sender, MqttMsgPublishedEventArgs e)
+        {
+        }
+    }
+}
