b117f8133343475861cf7c8d728584dc9ab94f4c
alaa <salaansgy4444@gmail.com>
2020-05-31 01:35:28 +0200

    final commit

--- Test public brocker/Form1.cs
+++ Test public brocker/Form1.cs
@@ -14,21 +14,28 @@ namespace Test_public_brocker
 {
     public partial class Form1 : Form
     {
-        MqttClient client;
+       static MqttClient client=new MqttClient("localhost", 1883, false, null, null, MqttSslProtocols.None);
         public Form1()
         {
             InitializeComponent();
-            client = new MqttClient("localhost", 1883, false, null, null, MqttSslProtocols.None);
             client.Connect(Guid.NewGuid().ToString());
         }
 
         private void send_Click(object sender, EventArgs e)
         {
             client.MqttMsgPublished += client_MqttMsgPublished;
-            ushort msgId = client.Publish("test/", Encoding.UTF8.GetBytes(X.Text +","+ Y.Text +","+ Z.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            ushort msgId= client.Publish("test/", Encoding.UTF8.GetBytes(X.Text + "," + Y.Text + "," + Z.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            //client.MqttMsgPublishReceived += client_MqttMsgrecieved;
+            //client.Subscribe(new string[] { "turnon/" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
         }
         private static void client_MqttMsgPublished(object sender, MqttMsgPublishedEventArgs e)
         {
+        }
+        private static void client_MqttMsgrecieved(object sender, MqttMsgPublishEventArgs e)
+        {
+            var msg = System.Text.Encoding.UTF8.GetString(e.Message);
+            MessageBox.Show("message Recieved" + msg);
+            client.MqttMsgPublished += client_MqttMsgPublished;            
         }
     }
 }
